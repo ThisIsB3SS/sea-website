896bb0dd7669d682a13705748aec135eb3f8b46e
Sebastien Escudero <sebastien.escudero@gmail.com>
2024-03-20 23:21:23 +0100

    added Header V1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
+import { Header } from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={inter.className}><ThemeProvider
+      <body className={inter.className}>
+        <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
+           <Header/>
             {children}
           </ThemeProvider></body>
     </html>
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import SEA_Logo from "../../public/Logo_SEA_Webstudio.svg"
+import Image from "next/image"
+
+export function Header(){
+
+    return (
+       <header className="fixed flex items-start w-full max-h-20 p-4">
+        <nav className="flex items-center justify-between px-4 max-h-20 w-full rounded-2xl border border-white border-opacity-25 backdrop-blur-2xl bg-opacity-25 bg-white overflow-visible  ">
+                <a href="/" className="flex justify-start">
+                    <Image src={SEA_Logo} alt="logo SEA Webstudio" className="w-20 transition-transform" />
+                    </a>
+                <div className="flex items-center space-x-4">
+                <a href="/about" className="hover:text-primary">About</a>
+                <a href="/services" className="hover:text-primary">Services</a>
+                <a href="/contact" className="hover:text-primary">Contact</a>
+        
+            </div>
+        </nav>
+
+       </header>
+    )
+}
