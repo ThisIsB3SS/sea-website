d63c656fa3276e3b88bed0ece075b69bb748e22f
Sebastien Escudero <sebastien.escudero@gmail.com>
2024-04-05 16:39:46 +0200

    fixed cta section color

--- src/components/ui/CtaSection.tsx
+++ src/components/ui/CtaSection.tsx
@@ -2,18 +2,18 @@ import '../../styles/custom.css';
 
 export function CtaSection() {
   return (
-    <div className="hero_cta_section z-10 w-full lg:p-16 mt-8 sm:mt-20 mb-10 mx-auto overflow-visible animate__animated animate__fadeIn">
+    <div className="hero_cta_section z-10 w-full lg:p-16 mt-8 sm:mt-20 mb-10 mx-auto  animate__animated animate__fadeIn">
       <a
         href="contact"
         className="flex justify-center items-center text-center"
       >
-        <div className="parent_hover overflow-visible flex flex-col sm:flex-row p-6 gap-4 justify-around items-center  min-h-full border border-gray-50 shadow-md rounded-full bg-white bg-opacity-50 backdrop-blur-xl transition-all duration-300 ease-in-out  hover:border-blue-500">
+        <div className="parent_hover group flex flex-col sm:flex-row p-6 gap-4 justify-around items-center  min-h-full border border-gray-50 shadow-md rounded-full bg-white bg-opacity-50 backdrop-blur-xl transition-all duration-300 ease-in-out  hover:border-blue-700">
           <h2 className="font-montserrat text-xl xl:text-2xl italic text-slate-800  ">
             Création, Développement,
             <br /> Stratégie Web & Mobile{' '}
           </h2>
           <div className="flex justify-center items-center gap-5 mt-2 lg:mt-0">
-            <span className="text-lg lg:text-2xl font-bold  text-blue-500 ">
+            <span className="text-lg lg:text-2xl font-bold  text-blue-500 group-hover:text-blue-700 transition-all duration-300 ease-in-out">
               Contactez-nous{' '}
             </span>
             <span className="w-10 h-10 p-2 flex content-center items-center bg-white rounded-full">
