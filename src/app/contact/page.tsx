040a1d78cff3456e20204a1968c1d751e846bb72
Sebastien Escudero <sebastien.escudero@gmail.com>
2024-04-13 00:51:16 +0200

    disabled fireworks-js for now

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import { useSubmit } from '@formspree/react';
 import { useForm } from 'react-hook-form';
 import { useEffect } from 'react';
 import Image from 'next/image';
-import { Fireworks } from '@fireworks-js/react';
+// import { Fireworks } from '@fireworks-js/react';
 type Inputs = {
   email: string;
   message: string;
@@ -11,63 +11,63 @@ type Inputs = {
   phone: string;
 };
 
-const fireScript = () => {
-  const container = document.querySelector('.canvas') as
-    | Element
-    | HTMLCanvasElement;
-  if (container) {
-    const fireworks = new Fireworks(container, {
-      /* options */
-      opacity: 0.5,
-      acceleration: 1.05,
-      friction: 0.97,
-      gravity: 1.5,
-      particles: 50,
-      traceLength: 3,
-      traceSpeed: 10,
-      explosion: 5,
-      intensity: 30,
-      flickering: 50,
-      lineStyle: 'round',
-      hue: {
-        min: 0,
-        max: 360,
-      },
-      delay: {
-        min: 30,
-        max: 60,
-      },
-      rocketsPoint: {
-        min: 50,
-        max: 50,
-      },
-      lineWidth: {
-        explosion: {
-          min: 1,
-          max: 3,
-        },
-        trace: {
-          min: 1,
-          max: 2,
-        },
-      },
-      brightness: {
-        min: 50,
-        max: 80,
-      },
-      decay: {
-        min: 0.015,
-        max: 0.03,
-      },
-      mouse: {
-        click: false,
-        move: false,
-        max: 1,
-      },
-    });
-    fireworks.start();
-  }
-};
+// const fireScript = () => {
+//   const container = document.querySelector('.canvas') as
+//     | Element
+//     | HTMLCanvasElement;
+//   if (container) {
+//     const fireworks = new Fireworks(container, {
+//       /* options */
+//       opacity: 0.5,
+//       acceleration: 1.05,
+//       friction: 0.97,
+//       gravity: 1.5,
+//       particles: 50,
+//       traceLength: 3,
+//       traceSpeed: 10,
+//       explosion: 5,
+//       intensity: 30,
+//       flickering: 50,
+//       lineStyle: 'round',
+//       hue: {
+//         min: 0,
+//         max: 360,
+//       },
+//       delay: {
+//         min: 30,
+//         max: 60,
+//       },
+//       rocketsPoint: {
+//         min: 50,
+//         max: 50,
+//       },
+//       lineWidth: {
+//         explosion: {
+//           min: 1,
+//           max: 3,
+//         },
+//         trace: {
+//           min: 1,
+//           max: 2,
+//         },
+//       },
+//       brightness: {
+//         min: 50,
+//         max: 80,
+//       },
+//       decay: {
+//         min: 0.015,
+//         max: 0.03,
+//       },
+//       mouse: {
+//         click: false,
+//         move: false,
+//         max: 1,
+//       },
+//     });
+//     fireworks.start();
+//   }
+// };
 export default function Contact(): React.JSX.Element {
   const {
     formState: { errors, isSubmitSuccessful, isSubmitting },
@@ -78,7 +78,7 @@ export default function Contact(): React.JSX.Element {
 
   useEffect(() => {
     if (isSubmitSuccessful) {
-      fireScript();
+      // fireScript();
     }
   }, [isSubmitSuccessful]);
 
