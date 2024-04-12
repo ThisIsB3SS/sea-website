d60ed66a09627a1f7a79cbe0ee16774f758d7115
Sebastien Escudero <sebastien.escudero@gmail.com>
2024-04-13 00:41:01 +0200

    fixed fireworks importation

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,54 +10,7 @@ type Inputs = {
   name: string;
   phone: string;
 };
-type FireworksOptions = {
-  opacity: number;
-  acceleration: number;
-  friction: number;
-  gravity: number;
-  particles: number;
-  traceLength: number;
-  traceSpeed: number;
-  explosion: number;
-  intensity: number;
-  flickering: number;
-  lineStyle: 'round' | 'square';
-  hue: {
-    min: number;
-    max: number;
-  };
-  delay: {
-    min: number;
-    max: number;
-  };
-  rocketsPoint: {
-    min: number;
-    max: number;
-  };
-  lineWidth: {
-    explosion: {
-      min: number;
-      max: number;
-    };
-    trace: {
-      min: number;
-      max: number;
-    };
-  };
-  brightness: {
-    min: number;
-    max: number;
-  };
-  decay: {
-    min: number;
-    max: number;
-  };
-  mouse: {
-    click: boolean;
-    move: boolean;
-    max: number;
-  };
-};
+
 const fireScript = () => {
   const container = document.querySelector('.canvas') as
     | Element
