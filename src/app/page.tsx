fb4bc410a50a1dac5452c55d358706e0e44cac0e
Sebastien Escudero <sebastien.escudero@gmail.com>
2024-04-16 21:55:17 +0200

    test 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,15 +20,15 @@ export default function Home() {
       >
         <div id="about"></div>
         <About />
-        {/* <div id="services"></div>
+        <div id="services"></div>
         <Services />
-        <CarouselBanner />
+        {/* <CarouselBanner /> */}
       </div>
-      <div id="projects"></div>
+      {/* <div id="projects"></div>
       <div className="projects-testimonials-wrapper z-0 w-full min-h-screen m-0 mb-6 flex flex-col justify-around ">
         <Projects />
-        <Testimonials /> */}
-      </div>
+        <Testimonials />
+      </div> */}
     </main>
   );
 }
