591921ef8ec386e7593116be44c82692a34280a8
Sebastien Escudero <sebastien.escudero@gmail.com>
2024-04-16 22:12:15 +0200

    test 7

--- src/components/sections/CarouselBanner.tsx
+++ src/components/sections/CarouselBanner.tsx
@@ -14,7 +14,7 @@ export function CarouselBanner() {
       <div className="carouselBanner_innerContainer">
         <div className="carouselBanner_innerContainer2">
           {/* Liste 1 */}
-          <Marquee
+          {/* <Marquee
             play={true}
             direction="right"
             speed={40}
@@ -34,10 +34,10 @@ export function CarouselBanner() {
                 </li>
               ))}
             </ul>
-          </Marquee>
+          </Marquee> */}
           <div className="separator border-t-2 border-gray-800 w-2/3 relative mx-auto mb-10"></div>
           {/* Liste 2 */}
-          <Marquee play={true} direction="left" speed={40} loop={0}>
+          {/* <Marquee play={true} direction="left" speed={40} loop={0}>
             <ul className="carouselBanner flex items-center justify-center gap-12">
               {Object.entries(logos.list2).map(([key, value]) => (
                 <li
@@ -54,7 +54,7 @@ export function CarouselBanner() {
                 </li>
               ))}
             </ul>
-          </Marquee>
+          </Marquee> */}
         </div>
       </div>
     </div>
